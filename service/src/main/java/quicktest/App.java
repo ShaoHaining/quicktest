91f0404586273fd28827cadad568667c1cf28a34
469304189@qq.com <469304189@qq.com>
2020-05-23 11:26:00 +0800

    时间：2020-05-20 描述：APP测试代码提交 提交人：海之宁静

--- service/src/main/java/quicktest/App.java
+++ service/src/main/java/quicktest/App.java
@@ -1,5 +1,11 @@
 package main.java.quicktest;
 
+import sun.util.resources.ga.LocaleNames_ga;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,12 +14,43 @@ public class App
 {
     public static void main( String[] args )
     {
+        Long beginTime = System.currentTimeMillis();
         BusinessService service = new BusinessService();
         System.out.println(service.testString());
         System.out.println( "Hello World!" );
 
         System.out.println(service.orderlyQueue("accb", 1));
         System.out.println(service.testQueue1());
+
+
+        System.out.println(System.currentTimeMillis());
+        Map<String,String> entryMap = new HashMap<>();
+        entryMap = System.getenv();
+        System.out.println(entryMap.get("MOZ_PLUGIN_PATH"));
+        Long endTime = System.currentTimeMillis();
+        System.out.println(endTime-beginTime);
+
+        String[] words = "Today is Sunday, I will stay at home.".split(" ");
+        Collection<String> wList = new LinkedList<>();
+        Collections.addAll(wList, words);
+        System.out.println(wList);
+
+
+        for (Map.Entry entry: System.getenv().entrySet()){
+            System.out.println(entry.getValue());
+        }
+        System.out.println("===================");
+
+        Map<String, String> mapDemo = new HashMap<>();
+        mapDemo.put("zhouyi", "Mon");
+        mapDemo.put("zhouer", "Tus");
+        mapDemo.put("zhousan", "Thus");
+
+        for (Map.Entry entry: mapDemo.entrySet()){
+            System.out.println(entry.getValue());
+        }
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!");
+
 
 
 
